0ecdb8148c8f844f275f98f62e136fcb3ac9717d
ajarazet <Alba.Jarazet@ellucian.com>
2023-08-21 23:21:33 -0600

    Add toggle & fix text areas

--- src/pages/UploadItem.jsx
+++ src/pages/UploadItem.jsx
@@ -15,12 +15,28 @@ const UploadItem = () => {
         <div className="upload-text-area">
           <div className="description-area area">
             <span>Description</span>
-
-            <textarea className="description-text" />
+            <textarea
+              placeholder="Less than 200 words."
+              className="description-text"
+            />
           </div>
           <div className="instructions-area area">
+            <div className="toggle-container">
+              <div className="toggle-btn">
+                <label className="switch">
+                  <input type="checkbox" />
+                  <span className="slider round"></span>
+                </label>
+              </div>
+              <div className="switch-text">
+                <span>Personalization</span>
+              </div>
+            </div>
             <span>Instructions for buyer</span>
-            <textarea className="instructions-text" />
+            <textarea
+              placeholder="Ex: Please explain how you will want your present to be wrapped"
+              className="instructions-text"
+            />
           </div>
         </div>
 
