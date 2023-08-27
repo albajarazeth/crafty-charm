438f74ae698393eb97c109aee52fd4b0baeedce6
ajarazet <Alba.Jarazet@ellucian.com>
2023-08-27 11:29:42 -0600

    Add register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FcGoogle } from "react-icons/fc";
+import "./Register.css";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const [email, setEmail] = useState("");
 
-export default Register
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setEmail(e.target.value);
+  };
+  return (
+    <section className="login-container">
+      <div className="login-card">
+        <div>
+          <form className="login-form" onSubmit={(e) => submitHandler(e)}>
+            <label htmlFor="username">Register </label>
+            <input
+              className="email-input"
+              id="username"
+              type="text"
+              placeholder="Email *"
+            />
+            <input
+              className="email-input"
+              id="username"
+              type="text"
+              placeholder="Password *"
+            />
+            <button className="primary-btn continue-btn " type="submit">
+              Continue
+            </button>
+          </form>
+          <div className="google login">
+            <span className="span">or register with</span>
+            <button className="primary-btn">
+              <div className="g-content">
+                <FcGoogle size={30} /> <div className="g-text">Google</div>
+              </div>
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Register;
